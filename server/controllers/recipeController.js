8cd11d9996fea126d59700b26ab7f93d43d8ff0a
00015881 <00015881@wiut.uz>
2024-03-13 14:00:48 +0500

    Worked with Categories and completed the most part of project

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,7 +1,63 @@
+require('../models/database');
+const Category = require('../models/category');
+
 /**
- * GET /
+ * Get /
  * Homepage
  */
-exports.homepage = async(req, res)=> {
-    res.render('index', { title: 'Cooking Blog - Homepage!'});
+exports.homepage = async (req, res) => {
+    try{
+
+        const limitNumber = 3;
+        const categories = await Category.find({}).limit(limitNumber);;
+
+        res.render('index', { title: 'Cooking Blog - Homepage!', categories: categories });
+    }
+    catch (error) {
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
 }
+
+/**
+ * Get Categories/
+ * Categories
+ */
+exports.exploreCategories = async (req, res) => {
+    try{
+
+        const limitNumber = 3;
+        const categories = await Category.find({}).limit(limitNumber);;
+
+        res.render('categories', { title: 'Cooking Blog - Categories!', categories: categories });
+    }
+    catch (error) {
+        res.status(500).send({message: error.message || "Error Occured"});
+    }
+}
+
+
+
+// async function insertDummycategoryData() {
+//     try {
+//         await Category.insertMany([
+//             {
+//                 "name": "Thai",
+//                 "image": "https://i.pinimg.com/originals/1f/23/ef/1f23efa9c6f7662eda40fc10e308f2d4.png"
+//             },
+//             {
+//                 "name": "American",
+//                 "image": "https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_auto,w_1500,ar_3:2/k%2FEdit%2Fshutterstock_284608058"
+//             },
+//             {
+//                 "name": "Chinese",
+//                 "image": "https://wallpaperaccess.com/full/4337523.jpg"
+//             },
+            
+//         ]);
+//     } catch (error) {
+//         console.log('err', error);
+//     }
+// }
+
+// // Call the function to insert dummy data
+// insertDummycategoryData();
--- server/models/database.js
+++ server/models/database.js
@@ -9,4 +9,3 @@ db.once('open', function(){
 
 // Models
 require('./category');
-require('./Recipe');
