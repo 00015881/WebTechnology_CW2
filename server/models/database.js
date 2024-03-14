444fff7717370dcb6f50efdebbe33cad9519d947
00015881 <00015881@wiut.uz>
2024-03-14 22:55:08 +0500

    edited database js

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://wiut00015881:<password>@cluster0.9qyy3xo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://wiut00015881:<Sora2005>@cluster0.9qyy3xo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
